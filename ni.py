ce7c7c18df5936dec9c8029fbdae53b8115ff09f
Alexander Wolosewicz <112141084+awolosewicz@users.noreply.github.com>
2025-12-03 17:34:04 -0600

    Add src IP address to packet

--- ni.py
+++ ni.py
@@ -582,11 +582,11 @@ class NICmd(cmd.Cmd):
                 print(warn_str)
                 return
         if type(dest) == IPv4Address:
-            pkt = IP(dst=str(dest))/NIHeader(level=self.nicxt.lattice.element_ids[str(level)],
+            pkt = IP(src=str(self.host.address), dst=str(dest))/NIHeader(level=self.nicxt.lattice.element_ids[str(level)],
                                              enc=encrypted, pkt_type=pkt_type, session=session,
                                              sig=signature)/Raw(load=rawdata.encode())
         elif type(dest) == IPv6Address:
-            pkt = IPv6(dst=str(dest))/NIHeader(level=self.nicxt.lattice.element_ids[str(level)],
+            pkt = IPv6(src=str(self.host.address), dst=str(dest))/NIHeader(level=self.nicxt.lattice.element_ids[str(level)],
                                              enc=encrypted, pkt_type=pkt_type, session=session,
                                              sig=signature)/Raw(load=rawdata.encode())
         send(pkt)
