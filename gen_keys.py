d3fae143f5be37e3e9e13107be3488b232f33750
Alexander Wolosewicz <112141084+awolosewicz@users.noreply.github.com>
2025-12-03 21:17:59 -0600

    Add hosts to keygen

--- gen_keys.py
+++ gen_keys.py
@@ -3,7 +3,7 @@ from nacl.signing import SigningKey, VerifyKey
 import base64
 
 if __name__ == "__main__":
-    for user in ["admin", "alice", "bob", "charlie", "eve"]:
+    for user in ["admin", "alice", "bob", "charlie", "eve", "pb", "n1", "n2"]:
         print(f"Keys for user: {user}")
         # Generate a new public/private key pair
         private_key = PrivateKey.generate()
